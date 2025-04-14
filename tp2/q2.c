7c64d6990231ed76e96972491985e13a5cfba8e1
lucasgrossi2 <lucasgrossi2005@gmail.com>
2025-04-14 19:48:52 -0300

    questão 2

--- tp2/q2.c
+++ tp2/q2.c
@@ -16,13 +16,88 @@ typedef struct {
     char** listed_in;
 } shows;
 
+char* separador(char* line){
+    char* token = malloc(1024 * sizeof(char));
+    if(line[0] == ','){
+        token = "NaN";
+        return token;
+    } else {
+        int counter = 0;
+        while(line[counter] != ','){
+            token[counter] = line[counter];
+            counter++;
+        }
+        token[counter] = '\0'; 
+
+        int offset = counter + 1;
+        memmove(line, line + offset, strlen(line) - offset + 1);  
+        return token;
+    }
+}
+
+char separadorAspas(char* line){
+
+}
 
 shows ler(char* base_id) {
-    
+    FILE *file = fopen("disneyplus.csv", "r");
+    char line[1024];
+    while (fgets(line, sizeof(line), file)) {
+        line[strcspn(line, "\n")] = '\0';
+        char *firstField = strtok(line, ",");
+        if(strcmp(firstField, base_id) == 0){
+            shows show = {0};
+            char* token;
+            for(int i = 0; i < 11; i++){
+                switch(i){
+                    case 4:
+                    case 10:
+                        token = separadorAspas(line);
+                    default:
+                        token = separador(line);
+                }
+                switch(i){
+                    case 0: //id
+                        strcpy(show.show_id, token);
+                        break;
+                    case 1: //type
+                        strcpy(show.type, token);
+                        break;
+                    case 2: //title
+                        strcpy(show.title, token);
+                        break;
+                    case 3: //director
+                        strcpy(show.director, token);
+                        break;
+                    case 4: //cast !!!!!
+                        break;
+                    case 5: //country
+                        strcpy(show.country, token);
+                        break;
+                    case 6: //date_added
+                        strcpy(show.date_added, token);
+                        break;
+                    case 7: //release_year !!!!!
+                        int year = atoi(token);
+                        strcpy(show.release_year, year);
+                        break;
+                    case 8: //rating
+                        strcpy(show.rating, token);
+                        break;
+                    case 9: //duration
+                        strcpy(show.duration, token);
+                        break;
+                    case 10: //listed_in !!!!!
+                        break;
+                }
+            }
+        }
+    }
 }
 
 void imprimir(char* base_id) {
-
+    ler(base_id);
+    //printf ........
 }
 
 int main() {
